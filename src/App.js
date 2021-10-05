42dbe90dd56c8ef01b5486e1e3c9fe725d9a2122
CORP\singhs137 <saumya.singh137@auspost.com.au>
2021-10-05 16:56:03 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://brandwrapped.s3.ap-southeast-2.amazonaws.com/images/test.jpg"  alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
